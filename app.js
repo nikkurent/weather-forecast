685165e828a70111b6977055b90750729281947f
Nik Kurent <nik.kurent1@gmail.com>
2021-01-18 19:08:38 +0100

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input')
 const ul = document.querySelector('ul');
-const apiKey = ApiKey;
+const apiKey = "aeb1e7ef095f34b93c1e98be0a5cd9f8";
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
